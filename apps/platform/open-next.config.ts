59edf823039365e188928b684e5af57a6d99305f
Kanak <kanakkholwal@gmail.com>
2025-09-02 23:17:13 +0530

    fix

--- apps/platform/open-next.config.ts
+++ apps/platform/open-next.config.ts
@@ -1,27 +1,32 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import { purgeCache } from "@opennextjs/cloudflare/overrides/cache-purge/index";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
-// import doShardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
+import doShardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
+
+
 
 export default defineCloudflareConfig({
-  enableCacheInterception: true,
-  // incrementalCache: kvIncrementalCache,
-  tagCache: d1NextTagCache,
+  incrementalCache: withRegionalCache(r2IncrementalCache, { mode: "long-lived" }),
   queue: doQueue,
   // This is only required if you use On-demand revalidation
-  // tagCache: doShardedTagCache({
-  //   baseShardSize: 12,
-  //   regionalCache: true, // Enable regional cache to reduce the load on the DOs
-  //   regionalCacheTtlSec: 5, // The TTL for the regional cache
-  //   regionalCacheDangerouslyPersistMissingTags: true, // Enable this to persist missing tags in the regional cache
-  //   shardReplication: {
-  //     numberOfSoftReplicas: 4,
-  //     numberOfHardReplicas: 2,
-  //     regionalReplication: {
-  //       defaultRegion: "enam",
-  //     },
-  //   },
-  // }),
+  tagCache: doShardedTagCache({
+    baseShardSize: 12,
+    regionalCache: true, // Enable regional cache to reduce the load on the DOs
+    regionalCacheTtlSec: 5, // The TTL for the regional cache
+    regionalCacheDangerouslyPersistMissingTags: true, // Enable this to persist missing tags in the regional cache
+    shardReplication: {
+      numberOfSoftReplicas: 4,
+      numberOfHardReplicas: 2,
+      regionalReplication: {
+        defaultRegion: "enam",
+      },
+    },
+  }),
+  // Disable this if you want to use PPR
+  enableCacheInterception: true,
+  // you can also use the `durableObject` option to use a durable object as a cache purge
+  cachePurge: purgeCache({ type: "direct" }),
   // Disable this if you want to use PPR
 });
